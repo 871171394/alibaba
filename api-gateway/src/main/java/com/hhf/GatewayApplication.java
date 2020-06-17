092bb2ef6bc22497b8b8407522d463c268f54da3
87117 <871171394@qq.com>
2020-06-17 18:10:24 +0800

    gateway

---
+++ api-gateway/src/main/java/com/hhf/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.hhf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Huang.Hua.Fu
+ * @date 2020/6/17 17:36
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
